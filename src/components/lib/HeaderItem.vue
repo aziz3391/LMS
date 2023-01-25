2863b83cf8ee3eec1cc54b3db233831fe8287277
Abdulaziz3391 <azizjon3391@gmail.com>
2023-01-25 11:44:38 +0500

    initial styles are ready and HeaderItem is also ready

---
+++ src/components/lib/HeaderItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <div class="firstitem">
+      <img class="imghead" src="/src/assets/img/logo.png" alt="School Logo" />
+      <ul class="ulhead">
+        <li>Курсы</li>
+        <li>О нас</li>
+        <li>Блог</li>
+        <li>Ещё</li>
+      </ul>
+    </div>
+
+    <button class="btnhead">Войти</button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "/src/styles/components/Header.scss";
+</style>
